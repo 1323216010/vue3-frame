6a74e53bf23493f56a99c03831c8161e72b4ae4d
13232160100 <ypc0100@qq.com>
2023-01-25 01:36:31 +0800

    增加操作提示

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import ElementPlus from 'element-plus'
 import locale from 'element-plus/lib/locale/lang/zh-cn' // 中文语言
 import elementIcons from '@/components/SvgIcon/svgicon'
 import { parseTime, resetForm, addDateRange, handleTree, selectDictLabel, selectDictLabels } from '@/utils/ruoyi.js'
+
+// 注册指令
+import plugins from './plugins' // plugins
 import { download } from '@/utils/request'
 
 const app = createApp(App);
@@ -14,7 +17,7 @@ app.config.globalProperties.handleTree = handleTree
 app.config.globalProperties.download = download
 
 app.use(router);
-
+app.use(plugins);
 // 使用element-plus 并且设置全局的大小
 app.use(ElementPlus, {
     locale: locale,
