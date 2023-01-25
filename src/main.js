c6a27d6cbe892293dd56be52542c91439c7072b1
13232160100 <ypc0100@qq.com>
2023-01-26 00:54:38 +0800

    调整样式

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import ElementPlus from 'element-plus'
-import locale from 'element-plus/lib/locale/lang/zh-cn' // 中文语言
+import 'element-plus/dist/index.css'
 import elementIcons from '@/components/SvgIcon/svgicon'
 import { parseTime, resetForm, addDateRange, handleTree, selectDictLabel, selectDictLabels } from '@/utils/ruoyi.js'
 
@@ -23,12 +22,5 @@ app.use(store)
 app.use(router);
 app.use(plugins);
 app.use(elementIcons)
-// 使用element-plus 并且设置全局的大小
-app.use(ElementPlus, {
-    locale: locale,
-    // 支持 large、default、small
-    size: 'default'
-})
-  
 
 app.mount('#app');
